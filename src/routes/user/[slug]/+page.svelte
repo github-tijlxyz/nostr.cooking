1fd39fc5d9b8503cfbd60e2f59d92454f245f9db
github-tijlxyz <123159729+github-tijlxyz@users.noreply.github.com>
2023-09-29 20:00:35 +0200

    css fixes

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -82,7 +82,7 @@
 
         <a
           href={`/user/${nip19.npubEncode(hexpubkey)}/lists`}
-          class="text-gray-500 hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10"
+          class="text-gray-500 rounded-r-lg hover:text-gray-700 group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10"
         >
           <span>Lists</span>
           <span aria-hidden="true" class="bg-transparent absolute inset-x-0 bottom-0 h-0.5" />
