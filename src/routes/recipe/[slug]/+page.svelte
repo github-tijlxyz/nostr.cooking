cf5a92705e9e1e44e215827f120e45fd8ce102b7
github-tijlxyz <123159729+github-tijlxyz@users.noreply.github.com>
2023-09-29 17:02:14 +0200

    add copy naddr

--- src/routes/recipe/[slug]/+page.svelte
+++ src/routes/recipe/[slug]/+page.svelte
@@ -17,6 +17,20 @@
 
   let event: NDKEvent;
   let zapModal = false;
+  let naddr: string = "";
+  let didCopy = false;
+
+  async function copyNaddr() {
+    const type = "text/plain";
+    const blob = new Blob([naddr], { type });
+    const data = [new ClipboardItem({ [type]: blob })];
+    navigator.clipboard.write(data).then(() => {
+      didCopy = true
+      setTimeout(() => {
+        didCopy = false
+      }, 2500)
+    })
+  }
 
   async function zapEvt(amount: number, message: string) {
     zapModal = false;
@@ -36,6 +50,13 @@
   onMount(async () => {
     if ($page.params.slug.startsWith('naddr1')) {
       const b = nip19.decode($page.params.slug).data;
+      naddr = nip19.naddrEncode({
+        // @ts-ignore
+        identifier: b.identifier,
+        // @ts-ignore
+        pubkey: b.pubkey,
+        kind: 30023,
+      })
       let e = await $ndk.fetchEvent({
         // @ts-ignore
         '#d': [b.identifier],
@@ -50,6 +71,13 @@
       let e = await $ndk.fetchEvent($page.params.slug);
       if (e) {
         event = e;
+        naddr = nip19.naddrEncode({
+          // @ts-ignore
+          identifier: e.tags.find((z) => z[0] == 'd')?.[1],
+          // @ts-ignore
+          kind: e.kind,
+          pubkey: e.author.hexpubkey
+        });
         const c = nip19.naddrEncode({
           // @ts-ignore
           identifier: e.tags.find((z) => z[0] == 'd')?.[1],
@@ -87,6 +115,7 @@
           <a class="underline" href="/user/{event.author.npub}"
             >{#await event.author?.fetchProfile()}...{:then result}{#if result !== null && result.name}{result.name}{:else}...{/if}{/await}</a
           >
+           &nbsp;•&nbsp; <a class="underline cursor-pointer" on:click={copyNaddr}>{#if didCopy}copied!{:else}copy naddr{/if}</a>
           {#if event.author.hexpubkey == $userPublickey}
             &nbsp;•&nbsp; <a href={`/fork/${event.id}`}>Edit</a>
           {:else if $userPublickey}
