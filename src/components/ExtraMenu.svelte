cca0cd6c6f39fd0fae99d33a12bf9e50e5fe4a06
github-tijlxyz <123159729+github-tijlxyz@users.noreply.github.com>
2023-10-10 16:37:49 +0200

    add image uploader and logo

--- src/components/ExtraMenu.svelte
+++ src/components/ExtraMenu.svelte
@@ -43,15 +43,15 @@
   >
     <div class="py-1" role="none">
       <AddBookmark {event} close={closeSelf} />
-      {#if event.pubkey == $userPublickey}
-        <button
-          on:click={() => {
-            copyNaddr();
-            closeSelf();
-          }}
-          class="text-gray-700 w-full text-left hover:bg-gray-100 hover:text-gray-900 block px-4 py-2"
-          ><span
-            ><svg
+      <!-- {#if event.pubkey == $userPublickey} -->
+      <button
+        on:click={() => {
+          copyNaddr();
+          closeSelf();
+        }}
+        class="text-gray-700 w-full text-left hover:bg-gray-100 hover:text-gray-900 block px-4 py-2"
+        ><span
+          >{#if event.pubkey == $userPublickey}<svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -64,12 +64,25 @@
                 stroke-linejoin="round"
                 d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
               />
-            </svg>
+            </svg>{:else}<svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+              />
+            </svg>{/if}
 
-            {#if event.pubkey == $userPublickey}Edit{:else}Fork{/if}</span
-          ></button
-        >
-      {/if}
+          {#if event.pubkey == $userPublickey}Edit{:else}Fork{/if}</span
+        ></button
+      >
+      <!-- {/if} -->
       <button
         on:click={() => {
           copyNaddr();
