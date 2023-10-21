0aa8cfb5dfc3144816393a9ec187c6d282115d35
Tijl <123159729+github-tijlxyz@users.noreply.github.com>
2023-10-21 10:26:54 +0200

    fix translation

--- src/lib/translation.ts
+++ src/lib/translation.ts
@@ -1,6 +1,6 @@
 import { setCORS } from 'google-translate-api-browser';
 
-const gTranslate = setCORS('https://cors-anywhere.herokuapp.com/');
+const gTranslate = setCORS('https://corsproxy.io/?');
 
 export async function translate(language: string, string: string) {
   // @ts-ignore
