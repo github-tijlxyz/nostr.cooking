446b94d82db249653f34d3eeb1eb642e80e3d2e3
github-tijlxyz <123159729+github-tijlxyz@users.noreply.github.com>
2023-11-15 21:54:47 +0100

    add author copy

--- src/components/ExtraMenu.svelte
+++ src/components/ExtraMenu.svelte
@@ -10,15 +10,6 @@
   export let copyNaddr: () => void;
   export let closeSelf: () => void;
   export let naddr: string;
- 
-  async function copyNpub() {
-    const type = 'text/plain';
-    const blob = new Blob([event.author.npub], { type });
-    const data = [new ClipboardItem({ [type]: blob })];
-    navigator.clipboard.write(data).then(() => {
-      setTimeout(() => {}, 2500);
-    }); 
-  }
 
   function saveAsMD() {
     const blob = new Blob([event.content], { type: 'text/markdown' });
@@ -119,30 +110,6 @@
           Copy naddr</span
         ></button
       >
-      <button
-        on:click={() => {
-          copyNpub();
-          closeSelf();
-        }}
-        class="text-gray-700 w-full text-left hover:bg-gray-100 hover:text-gray-900 block px-4 py-2"
-        ><span
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="w-6 h-6 inline text-center mr-1"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-            />
-          </svg>
-          Copy author npub</span
-        ></button
-      >      
       <button
         on:click={() => {
           saveAsMD();
