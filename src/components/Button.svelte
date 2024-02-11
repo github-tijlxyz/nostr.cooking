258f48b9fb403579ecff4b42d5a08f0edab97376
Jack Chakany <jack@chaker.net>
2024-02-11 13:44:52 -0500

    Make buttons semibold by default

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -2,6 +2,6 @@
   export let primary = true;
 </script>
 
-<button class="text-white rounded-full {primary ? 'bg-[#EC4700]' : 'bg-black'} px-4 py-2.5 {$$props.class}" on:click>
+<button class="text-white rounded-full {primary ? 'bg-[#EC4700]' : 'bg-black'} px-4 py-2.5 font-semibold {$$props.class}" on:click>
   <slot />
 </button>
