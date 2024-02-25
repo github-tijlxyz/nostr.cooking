5ef74a7ce24b2c54e9f2a0990be8b0d37547e3f5
Jack Chakany <jack@chaker.net>
2024-02-24 20:59:45 -0500

    fix some styling issues with the recipe page

--- src/components/Recipe/Recipe.svelte
+++ src/components/Recipe/Recipe.svelte
@@ -133,8 +133,8 @@
         {:then lists}
         <div class="flex flex-col gap-2">
           {#each lists as list, index}
-            <div class="flex gap-14">
-              <p class="font-semibold">{list.getMatchingTags("title")[0][1]}</p>
+            <div class="flex gap-14 w-full">
+              <p class="font-semibold ">{list.getMatchingTags("title")[0][1]}</p>
               <input class="self-center" type="checkbox" on:change={() => toggleList(list.replaceableDTag())} checked={list.getMatchingTags("a").find((t) => t[1] == `${event.kind}:${event.author.pubkey}:${event.replaceableDTag()}`) ? true : false} />
             </div>
             {#if index === 0}
@@ -184,7 +184,7 @@
             <TotalZaps {event} />
           </div>
         </div>
-        <div on:click={() => dropdownActive = !dropdownActive}>
+        <div class="cursor-pointer" on:click={() => dropdownActive = !dropdownActive}>
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             <path d="M12 6C12.5523 6 13 5.55228 13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5C11 5.55228 11.4477 6 12 6Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
