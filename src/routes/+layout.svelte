543471d1fe4e6e2da28d6e5b0b4656872e32a903
github-tijlxyz <contact@tijl.xyz>
2024-02-26 17:06:52 +0100

    add margin (for mobile)

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -71,7 +71,7 @@
 
 <div class="h-[100%] scroll-smooth">
   <div class="flex h-screen">
-    <div class="container mx-auto flex-1 overflow-y-auto pt-3">
+    <div class="container mx-auto flex-1 overflow-y-auto pt-3 px-3">
       <Header />
       <div class="mt-6 pb-24">
         <slot />
