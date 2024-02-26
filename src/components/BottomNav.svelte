06f5f0db2ed0d012461954c56a5d6bac25be80d4
github-tijlxyz <contact@tijl.xyz>
2024-02-26 17:09:30 +0100

    make mobile bottombar work

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -1,19 +1,20 @@
 <script>
+    import { goto } from '$app/navigation';
   import { faBookmark, faList, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
 </script>
 
 <div class="md:hidden pt-2 bg-white w-full fixed bottom-0 grid grid-cols-3 grid-rows-1 text-center">
-  <div class="flex flex-col">
+  <button on:click={() => goto("/")} class="flex flex-col">
     <Fa size="1.5x" icon={faMagnifyingGlass} />
     Discover
-  </div>
-  <div class="flex flex-col">
+  </button>
+  <button on:click={() => goto("/")} class="flex flex-col">
     <Fa size="1.5x" icon={faList} />
     Categories
-  </div>
-  <div class="flex flex-col">
+  </button>
+  <button on:click={() => goto("/bookmarks")} class="flex flex-col">
     <Fa size="1.5x" icon={faBookmark} />
     Bookmarks
-  </div>
+  </button>
 </div>
