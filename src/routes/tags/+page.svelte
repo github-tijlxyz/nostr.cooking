b89b8d5f86a5b69248a81dd21bf94c735193682a
Jack Chakany <jack@chaker.net>
2024-02-27 09:55:54 -0500

    update categories page styling

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -9,32 +9,18 @@
 <div class="flex flex-col gap-4">
   <h1>All Categories</h1>
 
-  <div class="grid grid-cols-5">
+  <div class="grid grid-cols-6">
     {#each recipeTags as tag}
       <a href="/tag/{tag.title}" class="flex flex-col gap-2">
-        <!-- TODO: CHANGE TO WEBP -->
-        <img
-          alt="Image of {tag.title}"
-          src="/tags/{tag.title.toLowerCase()}.webp"
-          class="table w-20 h-20 bg-input rounded-full place-self-center"
-        />
+        <div class="table w-[120px] h-[120px] bg-input rounded-full place-self-center">
+          <div class="table-cell align-middle place-self-center text-center text-7xl">
+            {tag.emoji}
+          </div>
+        </div>
         <div class="place-self-center">{tag.title}</div>
       </a>
     {/each}
   </div>
-</div>
-
-<h1 class="text-3xl text-center mt-5 mb-0">All Tags</h1>
-
-<div class="text-xl text-center px-[10vw] pt-6 pb-2">
-  {#each recipeTags as recipeTag}
-    <a
-      href={`/tag/${recipeTag.title.toLowerCase().replaceAll(' ', '-')}`}
-      class="mb-3 inline-block hover:underline mx-3"
-    >
-      {#if recipeTag.emoji}{recipeTag.emoji} {recipeTag.title}{:else}{recipeTag.title}{/if}
-    </a>
-  {/each}
 </div>
 
 <div class="text-center text-lg">
