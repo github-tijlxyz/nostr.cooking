0f0ea384cc07c8d43b766bc0739b5152741d7cfb
Jack Chakany <jack@chaker.net>
2024-02-27 09:48:20 -0500

    goto recent by default

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
-  import { userPublickey } from '$lib/nostr';
-  import { nip19 } from 'nostr-tools';
 
   onMount(async () => {
-    if ($userPublickey !== '') {
-      goto(`/user/${nip19.npubEncode($userPublickey)}`);
-    } else {
-      goto(`/recent`);
-    }
+    goto(`/recent`);
   });
 </script>
