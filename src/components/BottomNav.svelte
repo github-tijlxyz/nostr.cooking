a369cab202d34f6d8d3e5042782f3af4729e50f8
Jack Chakany <jack@chaker.net>
2024-02-27 09:48:46 -0500

    update Bottom Navigation links

--- src/components/BottomNav.svelte
+++ src/components/BottomNav.svelte
@@ -1,20 +1,19 @@
 <script>
-    import { goto } from '$app/navigation';
   import { faBookmark, faList, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
   import Fa from 'svelte-fa';
 </script>
 
-<div class="md:hidden pt-2 bg-white w-full fixed bottom-0 grid grid-cols-3 grid-rows-1 text-center">
-  <button on:click={() => goto("/")} class="flex flex-col">
+<nav class="md:hidden pt-2 bg-white w-full fixed bottom-0 grid grid-cols-3 grid-rows-1 text-center">
+  <a href="/recent" class="flex flex-col">
     <Fa size="1.5x" icon={faMagnifyingGlass} />
     Discover
-  </button>
-  <button on:click={() => goto("/")} class="flex flex-col">
+  </a>
+  <a href="/tags" class="flex flex-col">
     <Fa size="1.5x" icon={faList} />
     Categories
-  </button>
-  <button on:click={() => goto("/bookmarks")} class="flex flex-col">
+  </a>
+  <a href="/bookmarks" class="flex flex-col">
     <Fa size="1.5x" icon={faBookmark} />
     Bookmarks
-  </button>
-</div>
+  </a>
+</nav>
