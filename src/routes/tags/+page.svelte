940422b298f7dc3109591402b7e883202170a161
Jack Chakany <jack@chaker.net>
2024-02-28 11:06:16 -0500

    increase category row spacing

--- src/routes/tags/+page.svelte
+++ src/routes/tags/+page.svelte
@@ -9,7 +9,7 @@
 <div class="flex flex-col gap-4">
   <h1>All Categories</h1>
 
-  <div class="grid grid-cols-6">
+  <div class="grid grid-cols-6 gap-8">
     {#each recipeTags as tag}
       <a href="/tag/{tag.title}" class="flex flex-col gap-2">
         <div class="table w-[120px] h-[120px] bg-input rounded-full place-self-center">
